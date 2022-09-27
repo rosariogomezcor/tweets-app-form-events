e4a65502275141d01f3d22f1c7cc1a4649368890
Rosario <rosariogomez639@gmail.com>
2022-09-27 15:26:31 -0300

    add logic

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const tweetForm = document.querySelector('#tweetForm'); 
+const tweetContainer = document.querySelector('#tweets'); 
 tweetForm.addEventListener('submit', function(e) {
-    console.log("submit"); 
     e.preventDefault(); 
+
+    const username = tweetForm.elements.username.value; 
+    const tweet = tweetForm.elements.tweet.value;  
+    
+    const newTweet = document.createElement('li'); 
+    const bTag = document.createElement('b'); 
+    bTag.append(username); 
+    newTweet.append(bTag); 
+    newTweet.append(`- ${tweet}`); 
+    tweetContainer.append(newTweet); 
 }); 
 
