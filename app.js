a069512154d4fde57a10a3ce7ee76f2de50c7efe
Rosario <rosariogomez639@gmail.com>
2022-09-27 15:30:43 -0300

    refactor code

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ tweetForm.addEventListener('submit', function(e) {
 
     const username = tweetForm.elements.username.value; 
     const tweet = tweetForm.elements.tweet.value;  
-    
+    addTweet(username, tweet); 
+}); 
+
+const addTweet = (username, tweet) => {
     const newTweet = document.createElement('li'); 
     const bTag = document.createElement('b'); 
     bTag.append(username); 
     newTweet.append(bTag); 
     newTweet.append(`- ${tweet}`); 
     tweetContainer.append(newTweet); 
-}); 
-
+}
